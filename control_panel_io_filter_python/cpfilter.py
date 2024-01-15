a9e919e928954b912eda764ab8e34c1e1cd9cab9
Roger Cheng <roger.random@outlook.com>
2024-01-15 11:19:41 -0800

    Get a sense of idle (~900K) & data backup (~40 max 2) per 100 messages (~920ms).

--- control_panel_io_filter_python/cpfilter.py
+++ control_panel_io_filter_python/cpfilter.py
@@ -10,18 +10,32 @@ with serial.Serial(port='/dev/cu.usbserial-AO002W1A',
     last_byte = control_panel.read()[0]
     last_byte_count = 1
     ack_count = 0
+    idle_count = 0
+    multibyte_wait = 0
+    wait_max = 0
     while(True):
-        # Read next byte
-        new_byte = control_panel.read()[0]
-
-        if (new_byte == 0x20):
-            # Keep count of ACK
-            ack_count += 1
-        elif (new_byte == last_byte and last_byte_count < 100):
-            # Keep count if byte unchanged for <100 repeats
-            last_byte_count += 1
+        if (control_panel.in_waiting == 0):
+            idle_count += 1
         else:
-            # Print out a status line of button matrix report byte + count of ACK
-            print(hex(last_byte) + " * " + str(last_byte_count) + "    0x20 ACK " + str(ack_count))
-            last_byte = new_byte
-            last_byte_count = 1
+            if control_panel.in_waiting > 1:
+                multibyte_wait += 1
+                if control_panel.in_waiting > wait_max:
+                    wait_max = control_panel.in_waiting
+            # Read next byte
+            new_byte = control_panel.read()[0]
+
+            if (new_byte == 0x20):
+                # Keep count of ACK
+                ack_count += 1
+            elif (new_byte == last_byte and last_byte_count < 100):
+                # Keep count if byte unchanged for <100 repeats
+                last_byte_count += 1
+            else:
+                # Print out a status line of button matrix report byte + count of ACK
+                print(hex(last_byte) + " * " + str(last_byte_count) + "    0x20 ACK " + str(ack_count) + "   IDLE " + str(idle_count) + "    Multibyte wait " + str(multibyte_wait) + " max " + str(wait_max))
+                last_byte = new_byte
+                last_byte_count = 1
+                ack_count = 0
+                idle_count = 0
+                multibyte_wait = 0
+                wait_max = 0
