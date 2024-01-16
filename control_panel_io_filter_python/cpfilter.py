7a3f69caba1cf53ff133ba0a8976b93c554a23d1
Roger Cheng <roger.random@outlook.com>
2024-01-16 15:06:01 -0800

    Clarify values used by control panel filter

--- control_panel_io_filter_python/cpfilter.py
+++ control_panel_io_filter_python/cpfilter.py
@@ -4,14 +4,16 @@ serial_parameters = {
     "baudrate":250000,
     "bytesize":serial.EIGHTBITS,
     "parity":serial.PARITY_EVEN,
-    "stopbits":1
+    "stopbits":serial.STOPBITS_ONE
 }
+
+CONTROL_PANEL_ACK       = 0x20  # Acknowledgement of main board commands
+CONTROL_PANEL_NO_BUTTON = 0x80  # Default scan code for "no button pressed"
 
 with serial.Serial(
     port='/dev/cu.usbserial-ABSCE0EZ', **serial_parameters) as main_board, serial.Serial(
     port='/dev/cu.usbserial-AO002W1A', **serial_parameters) as control_panel:
-    last_byte = 0x80 # Default "no button pressed"
-    ack_count = 0
+    previous_control_panel_byte = CONTROL_PANEL_NO_BUTTON
     idle_count = 0
     mb_count = 0
     while(True):
@@ -30,20 +32,19 @@ with serial.Serial(
 
         if (control_panel.in_waiting > 0):
             # Read available data
-            new_bytes = control_panel.read(control_panel.in_waiting)
+            new_control_panel_bytes = control_panel.read(control_panel.in_waiting)
 
-            for new_byte in new_bytes:
-                if (new_byte == 0x20):
-                    # Keep count of ACK
-                    ack_count += 1
-                elif (new_byte != last_byte):
-                    print(hex(new_byte), end=' ')
-                    if new_byte == 0x80:
+            for new_control_panel_byte in new_control_panel_bytes:
+                if (new_control_panel_byte == CONTROL_PANEL_ACK):
+                    None # Ignore ACK for now
+                elif (new_control_panel_byte != previous_control_panel_byte):
+                    print(hex(new_control_panel_byte), end=' ')
+                    if new_control_panel_byte == CONTROL_PANEL_NO_BUTTON:
                         print("button released")
-                    elif new_byte == 0x40:
+                    elif new_control_panel_byte == 0x40:
                         print("expected but unknown")
-                    elif new_byte >= 0x89 and new_byte <=0xCC:
+                    elif new_control_panel_byte >= 0x89 and new_control_panel_byte <=0xCC:
                         print("button scan code")
                     else:
                         print("-- NOVEL VALUE? --")
-                    last_byte = new_byte
+                    previous_control_panel_byte = new_control_panel_byte
