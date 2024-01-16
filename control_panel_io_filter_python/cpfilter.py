02aee5b343cf5722514fec9da95c426f432a99d6
Roger Cheng <roger.random@outlook.com>
2024-01-16 14:57:47 -0800

    Second serial port counts main board data burst size

--- control_panel_io_filter_python/cpfilter.py
+++ control_panel_io_filter_python/cpfilter.py
@@ -1,14 +1,33 @@
 import serial
 
-with serial.Serial(port='/dev/cu.usbserial-AO002W1A',
-                   baudrate=250000,
-                   bytesize=serial.EIGHTBITS,
-                   parity=serial.PARITY_EVEN,
-                   stopbits=1,
-                   timeout=0.1) as control_panel:
+serial_parameters = {
+    "baudrate":250000,
+    "bytesize":serial.EIGHTBITS,
+    "parity":serial.PARITY_EVEN,
+    "stopbits":1
+}
+
+with serial.Serial(
+    port='/dev/cu.usbserial-ABSCE0EZ', **serial_parameters) as main_board, serial.Serial(
+    port='/dev/cu.usbserial-AO002W1A', **serial_parameters) as control_panel:
     last_byte = 0x80 # Default "no button pressed"
     ack_count = 0
+    idle_count = 0
+    mb_count = 0
     while(True):
+        if main_board.in_waiting > 0:
+            # Read available data
+            new_bytes = main_board.read(main_board.in_waiting)
+            for new_byte in new_bytes:
+                mb_count += 1
+        elif mb_count > 0:
+            idle_count += 1
+
+        if idle_count > 100000:
+            print("Main board sent",str(mb_count),"bytes")
+            mb_count = 0
+            idle_count = 0
+
         if (control_panel.in_waiting > 0):
             # Read available data
             new_bytes = control_panel.read(control_panel.in_waiting)
