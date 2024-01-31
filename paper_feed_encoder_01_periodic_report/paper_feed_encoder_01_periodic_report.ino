ecf48649cfd5ea948f31ed55619f692cf9ddb169
Roger Cheng <roger.random@outlook.com>
2024-01-31 12:50:07 -0800

    Update comments from basic example

--- paper_feed_encoder_01_periodic_report/paper_feed_encoder_01_periodic_report.ino
+++ paper_feed_encoder_01_periodic_report/paper_feed_encoder_01_periodic_report.ino
@@ -1,7 +1,12 @@
 /* 
  * Quick tool for exploring Canon Pixma MX340
  * 
- * 
+ * Continuously reads the quadrature encoder reporting rotation of
+ * paper feed shaft, plus two photo interrupter sensors associated
+ * with paper operations. Performs no processing, merely report
+ * status at regular intervals (millisPerPrint) to the serial port
+ * in comma-separated values (CSV) format. Intended for export to
+ * some other data analysis software.
  * 
  * Derived from Encoder Library - Basic Example
  * http://www.pjrc.com/teensy/td_libs_Encoder.html
@@ -11,17 +16,20 @@
 
 #include <Encoder.h>
 
-// Change these two numbers to the pins connected to your encoder.
-//   Best Performance: both pins have interrupt capability
-//   Good Performance: only the first pin has interrupt capability
-//   Low Performance:  neither pin has interrupt capability
+// For best performance, select two interrupt capable pins on the
+// microcontroller. For the ATmega328 on board an Arduino Nano,
+// they are pins 2 and 3.
 Encoder myEnc(2, 3);
-//   avoid using pins with LEDs attached
 
-const int paperPin = 6;
-const int gearPin = 8;
+// Pins to read status of photo interrupter sensors. Any digital
+// input pin will suffice.
+const int paperPin = 6; // Probably reports paper presence
+const int gearPin = 8; // Something in a gearbox of yet-unknown purpose
 
+// When to print to serial port.
 unsigned long millisNextPrint;
+
+// Time period between prints
 unsigned long millisPerPrint = 10;
 
 void setup() {
